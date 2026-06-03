724f88738288b6f74631bf3ec13528eaabfb2b5f
Judi-eng <tlsdmswn27@gmail.com>
2026-06-03 15:46:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('reserveForm').addEventListener('submit', function(e){
-  e.preventDefault();
-  alert('상담 신청이 접수되었습니다.');
+e.preventDefault();
+alert('방문예약 신청이 접수되었습니다.');
 });
